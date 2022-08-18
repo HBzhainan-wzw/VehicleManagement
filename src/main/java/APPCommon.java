c62f78a2fe5c0360cecb6643201fe6bc4985acea
HBzhainan-wzw <zhiwen_wu@ucsb.edu>
2022-08-18 05:02:23 -0700

    Initial commit

---
+++ src/main/java/APPCommon.java
@@ -0,0 +1,7 @@
+//public class APPCommon {
+//    public static void main(String[] args) {
+//        BookService bookService = new BookServiceImpl();
+//        bookService.save();
+//
+//    }
+//}
---
+++ src/main/java/com/wzw/dao/VehicleDao.java
@@ -0,0 +1,7 @@
+package com.wzw.dao;
+
+public interface VehicleDao {
+
+    public void VehicleRegister(Integer vid, String plate, String VIN, String brand, String make, String color,
+                               String serveType, Integer numSeat, String vehicleCondition, Integer year);
+}
